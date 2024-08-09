472be0e0acd1828af2663679906ae7bccda95a40
user <user@SDAcourse.pl>
2024-08-09 13:04:36 +0200

    Second commit

--- management/management/settings.py
+++ management/management/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'students'
+
+    'django_extensions',
+
+    'students',
 ]
 
 MIDDLEWARE = [
@@ -121,7 +124,12 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [(os.path.join(BASE_DIR,'static'))]
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = '/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = '/'
